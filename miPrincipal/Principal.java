a5611bd1959da701e22c9bcc6bc6ea77e98b83a8
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2026-02-11 17:57:05 +0000

    Clase Punto

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,30 @@
 package miPrincipal;
 
 public class Principal {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+   
 
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        //System.out.println("Hola Mundo");
+        //Crear tres Objetos de la clase Punto
+
+        Punto p1 = new Punto(); //invocando al constructor vacio
+        Punto p2 = new Punto(1,1);
+        Punto p3 = new Punto(4,5);
+
+        p1.fijarX(3);
+        p1.fijarY(4);
+
+        System.out.print("x = "+p1.LeerX()+"  ");
+        System.out.println("y = "+p1.LeerY());
+
+        System.out.print("x = "+p2.LeerX()+"  ");
+        System.out.println("y = "+p2.LeerY());
+
+        p3.fijarY(4);
+
+        System.out.print("x = "+p3.LeerX()+"  ");
+        System.out.println("y = "+p3.LeerY());
+
+
     }
 }
---
+++ miPrincipal/Punto.java
@@ -0,0 +1,30 @@
+package miPrincipal;
+public class Punto {
+    //Atributos
+    private int x; //coordenada x
+    private int y; //coordenada y
+    //Métodos constructores
+    public Punto(){
+        x = 0;
+        y = 0;
+
+    }
+    public Punto(int x_, int y_){
+        x = x_;
+        y = y_;
+    }
+
+    public void fijarX(int valorX){
+        x = valorX;
+    }
+    public void fijarY(int valorY){
+        y = valorY;
+    }
+    public int LeerX(){
+        return x;
+    }
+    public int LeerY(){
+        return y;
+    }
+
+}
